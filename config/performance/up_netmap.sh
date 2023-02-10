2b27b685781ee71703725a3ce8a6265742d4e517
Konstantin Kogdenko <k.kogdenko@gmail.com>
2023-02-10 15:51:11 +0300

    lrf: configperformance up

--- config/performance/up_netmap.sh
+++ config/performance/up_netmap.sh
@@ -4,12 +4,19 @@
 nic()
 {
 	ifconfig $1 promisc up                                    
-	ethtool -L $1 combined 1                                                                                                                                
+	ethtool -L $1 combined 1
 	ethtool -G $1 rx 2048 tx 2048
 }
 
 rmmod ixgbe
 insmod ./netmap.ko
 insmod ./ixgbe/ixgbe.ko
+
+nic ix0a
+nic ix0b
 nic ix1a
+nic ix2b
+nic ix2a
+nic ix2b
 nic ix3a
+nic ix3b
